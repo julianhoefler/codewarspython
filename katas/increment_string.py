fa8b2c1024d796a427f7a4967f636bdcdc772f47
julianhoefler <96192975+julianhoefler@users.noreply.github.com>
2021-12-18 15:40:42 +0100

    add solution to increment_string kata

---
+++ katas/increment_string.py
@@ -0,0 +1,14 @@
+import re
+
+# Kata : https://www.codewars.com/kata/54a91a4883a7de5d7800009c
+
+def increment_string(string):
+    numbers = re.findall('([0-9]+)$', string)
+
+    if len(numbers) != 0:
+        numbers_size = len(numbers[0])
+        string = string[:len(string) - numbers_size]
+        number_with_zeros = str(int(''.join(str(i) for i in numbers)) + 1).zfill(numbers_size)
+        return string + number_with_zeros
+    else:
+        return string + str(1)
